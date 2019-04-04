1c1a90f0a27277736d424c619e4c8ad0ea8245a7
Julian Gindi <julian@gindi.io>
2019-04-04 05:51:30 -0700

    working docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,46 @@
 FROM python:3.7-stretch 
 
+# I hate this but it might work
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+RUN touch ~/.bashrc && chmod +x ~/.bashrc
+
+# Creating app directories
+RUN mkdir /app
+WORKDIR /app
+
 # Install Node
-# RUN curl -sL https://deb.nodesource.com/setup_11.x | bash -
-# RUN apt-get install -y nodejs
-RUN mkdir -p /usr/local/nvm
-ENV NVM_DIR /usr/local/nvm
+RUN mkdir -p /root/.nvm
+ENV NVM_DIR /root/.nvm
 RUN curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
 # Install base Python packages we need
 RUN pip install pipenv
 
-RUN mkdir /app
-WORKDIR /app
-
+# Add package files and install
 ADD Pipfile /app
 ADD Pipfile.lock /app
 ADD package.json /app
 ADD package-lock.json /app
 ADD .nvmrc /app
 
+# Install pip packages
 RUN pipenv install
 
+# Install specified Node version
+RUN source ~/.bashrc && nvm install && nvm use
 
-RUN . $NVM_DIR/nvm.sh && nvm install 10.15
-RUN npm install
+# Install npm packages
+RUN mkdir -p superrentals/ember
+ADD superrentals/ember/package.json superrentals/ember
+ADD superrentals/ember/package-lock.json superrentals/ember
+RUN source ~/.bashrc && npm run postinstall
+
+# Add full repo
+ADD . /app
+
+# Build static assets
+RUN source ~/.bashrc && npm run build
+
+EXPOSE 8000
+
+CMD pipenv run python manage.py runserver
--- superrentals/settings.py
+++ superrentals/settings.py
@@ -18,194 +18,173 @@ DJANGO_ROOT = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(DJANGO_ROOT)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 TEMPLATE_DEBUG = DEBUG
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
     # Disable Django's own staticfiles handling in favour of WhiteNoise, for
     # greater consistency between gunicorn and `./manage.py runserver`. See:
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
-    'whitenoise.runserver_nostatic',
-
-    'rest_framework',
-    'rest_framework_json_api',
-    'django_filters',
-
+    "whitenoise.runserver_nostatic",
+    "rest_framework",
+    "rest_framework_json_api",
+    "django_filters",
     # Use custom staticfiles AppConfig in order to ignore ember html files.
-    'superrentals.apps.SuperRentalsStaticFilesConfig',
-    'superrentals',
+    "superrentals.apps.SuperRentalsStaticFilesConfig",
+    "superrentals",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'superrentals.urls'
+ROOT_URLCONF = "superrentals.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'debug': DEBUG,
+            "debug": DEBUG,
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'superrentals.wsgi.application'
+WSGI_APPLICATION = "superrentals.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASE_SSL = config('DATABASE_SSL', default=True, cast=bool)
+DATABASE_SSL = config("DATABASE_SSL", default=True, cast=bool)
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600,
-                                      ssl_require=DATABASE_SSL),
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=DATABASE_SSL)
 }
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
     },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},  # noqa
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},  # noqa
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"  # noqa
     },
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
 # Force SSL
-SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=True, cast=bool)
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-WHITENOISE_ROOT = os.path.join(DJANGO_ROOT, 'www')
-STATIC_ROOT = os.path.join(WHITENOISE_ROOT, 'static')
-STATIC_URL = '/static/'
+WHITENOISE_ROOT = os.path.join(DJANGO_ROOT, "www")
+STATIC_ROOT = os.path.join(WHITENOISE_ROOT, "static")
+STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(DJANGO_ROOT, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(DJANGO_ROOT, "static")]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Logging
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
+    "handlers": {
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
     },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
+    "loggers": {"testlogger": {"handlers": ["console"], "level": "INFO"}},
 }
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 10,
-    'EXCEPTION_HANDLER':
-        'rest_framework_json_api.exceptions.exception_handler',
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework_json_api.pagination.JsonApiPageNumberPagination',
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework_json_api.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser'
+    "PAGE_SIZE": 10,
+    "EXCEPTION_HANDLER": "rest_framework_json_api.exceptions.exception_handler",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework_json_api.pagination.JsonApiPageNumberPagination",
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework_json_api.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
     ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework_json_api.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework_json_api.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ),
-    'DEFAULT_METADATA_CLASS':
-        'rest_framework_json_api.metadata.JSONAPIMetadata',
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_json_api.filters.QueryParameterValidationFilter',
-        'rest_framework_json_api.filters.OrderingFilter',
-        'rest_framework_json_api.django_filters.DjangoFilterBackend',
-        'rest_framework.filters.SearchFilter',
+    "DEFAULT_METADATA_CLASS": "rest_framework_json_api.metadata.JSONAPIMetadata",
+    "DEFAULT_FILTER_BACKENDS": (
+        "rest_framework_json_api.filters.QueryParameterValidationFilter",
+        "rest_framework_json_api.filters.OrderingFilter",
+        "rest_framework_json_api.django_filters.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
     ),
-    'SEARCH_PARAM': 'filter[search]',
-    'TEST_REQUEST_RENDERER_CLASSES': (
-        'rest_framework_json_api.renderers.JSONRenderer',
+    "SEARCH_PARAM": "filter[search]",
+    "TEST_REQUEST_RENDERER_CLASSES": (
+        "rest_framework_json_api.renderers.JSONRenderer",
     ),
-    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
+    "TEST_REQUEST_DEFAULT_FORMAT": "vnd.api+json",
 }
 
 # Ember LiveReload Server
-LIVERELOAD_ENABLED = DEBUG and config('LIVERELOAD', default=False, cast=bool)
-LIVERELOAD_PORT = config('LIVERELOAD_PORT', cast=int,
-                         default=config('PORT', cast=int, default=8000)+100)
+LIVERELOAD_ENABLED = DEBUG and config("LIVERELOAD", default=False, cast=bool)
+LIVERELOAD_PORT = config(
+    "LIVERELOAD_PORT", cast=int, default=config("PORT", cast=int, default=8000) + 100
+)
 
 # Ember Configuration
 # Used to update superrentals/ember/config/environment.js at runtime.
 EMBER_CONFIG = {
     # ember-simple-leaflet-maps
     # See: https://joseph.is/2CirF9p
-    'ember-simple-leaflet-maps': {
-        'apiKey': config('MAPBOX_ACCESS_TOKEN', default=''),
-    }
+    "ember-simple-leaflet-maps": {"apiKey": config("MAPBOX_ACCESS_TOKEN", default="")}
 }
